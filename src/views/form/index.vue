0270c17ca6a7a5750d2bc22bf063b3e69b6f8675
AndyTiTi <458666482@qq.com>
2019-05-31 14:24:22 +0800

    test pre-commit

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -49,20 +49,20 @@
 
 <script>
 export default {
-  data() {
-    return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
-      }
-    }
-  },
+data() {
+return {
+form: {
+name: '',
+region: '',
+date1: '',
+date2: '',
+delivery: false,
+type: [],
+resource: '',
+desc: ''
+}
+}
+},
   methods: {
     onSubmit() {
       this.$message('submit!')
